5e02a025ad5634e501a5b765ab66ace71aba10a7
chengheming <994483868@qq.com>
2022-07-07 19:12:56 +0800

    1. html.

---
+++ Algorithm_C/Algorithm_C/SortingAlgorithmReview/InsertionSort.cpp
@@ -0,0 +1,25 @@
+//
+//  InsertionSort.cpp
+//  Algorithm_C
+//
+//  Created by HM C on 2022/7/7.
+//
+
+#include <stdio.h>
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void insertionSort(int nums[], int count) {
+    if (nums == nullptr || count <= 0) {
+        return;
+    }
+    
+    for (int i = 1; i < count; ++i) {
+        for (int j = i; j > 0 && nums[j - 1] > nums[j]; --j) {
+            swap(&nums[j - 1], &nums[j]);
+        }
+    }
+}
