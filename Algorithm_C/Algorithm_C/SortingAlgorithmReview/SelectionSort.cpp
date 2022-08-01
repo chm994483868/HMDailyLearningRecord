62bdc6e02225532df07f554444bd7d0e9af7d38a
chengheming <994483868@qq.com>
2022-08-01 19:06:50 +0800

    1. sort.

--- Algorithm_C/Algorithm_C/SortingAlgorithmReview/SelectionSort.cpp
+++ Algorithm_C/Algorithm_C/SortingAlgorithmReview/SelectionSort.cpp
@@ -192,3 +192,16 @@ void selectSort(int nums[], int count) {
         swap(&nums[minIndex], &nums[i]);
     }
 }
+
+void selectSort(int nums[], int count) {
+    if (nums == nullptr || count <= 0) {
+        return;
+    }
+    
+    for (int i = 0; i < count; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; j < count; ++j) {
+            
+        }
+    }
+}
