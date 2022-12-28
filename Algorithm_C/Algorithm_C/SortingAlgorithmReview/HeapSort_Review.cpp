6e5e24134aaebb7ac23ee2b8fc97879a93ffee62
chengheming <994483868@qq.com>
2022-12-28 19:01:48 +0800

    1. sort.

--- Algorithm_C/Algorithm_C/SortingAlgorithmReview/HeapSort_Review.cpp
+++ Algorithm_C/Algorithm_C/SortingAlgorithmReview/HeapSort_Review.cpp
@@ -1,11 +1,12 @@
 //
-//  HeapSort.cpp
+//  HeapSort_Review.cpp
 //  Algorithm_C
 //
-//  Created by HM C on 2022/7/13.
+//  Created by HM C on 2022/12/28.
 //
 
 #include <stdio.h>
+void maxHeapFixdown(int nums[], int i, int n);
 
 void swap(int *a, int *b) {
     int temp = *a;
@@ -20,42 +21,6 @@ void heapSort(int nums[], int count) {
     
     int i = (count - 1 - 1) / 2;
     
-    for (; i >= 0; --i) {
-        maxHeapFixdown(nums, i, count);
-    }
-    
-    for (i = count - 1; i >= 1; --i) {
-        swap(&nums[0], &nums[i]);
-        maxHeapFixdown(nums, 0, i);
-    }
-}
-
-void maxHeapFixdown(int nums[], int i, int n) {
-    int j = i * 2 + 1;
-    int temp = nums[i];
-    
-    while (j < n) {
-        if (j + 1 < n && nums[j + 1] > nums[j]) {
-            ++j;
-        }
-        
-        if (nums[j] <= temp) {
-            break;
-        }
-        
-        swap(&nums[i], &nums[j]);
-        
-        i = j;
-        j = i * 2 + 1;
-    }
-}
-
-void heapSort(int nums[], int count) {
-    if (nums == nullptr || count <= 0) {
-        return;
-    }
-    
-    int i = (count - 1 - 1) / 2;
     for (; i >= 0; --i) {
         maxHeapFixdown(nums, i, count);
     }
---
+++ Algorithm_C/Algorithm_C/SortingAlgorithmReview/InsertionSort_Review.cpp
@@ -0,0 +1,38 @@
+//
+//  InsertionSort_Review.cpp
+//  Algorithm_C
+//
+//  Created by HM C on 2022/12/28.
+//
+
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void insertSort(int nums[], int count) {
+    if (nums == nullptr || count <= 0) {
+        return;
+    }
+    
+    for (int i = 1; i < count; ++i) {
+        for (int j = i; j > 0 && nums[j - 1] > nums[j]; --j) {
+            swap(&nums[j - 1], &nums[j]);
+        }
+    }
+}
+
+void insertSort_1(int nums[], int count) {
+    if (nums == nullptr || count <= 0) {
+        return;
+    }
+    
+    for (int i = 1; i < count; ++i) {
+        for (int j = i; j > 0 && nums[j - 1] > nums[j] ; --j) {
+            swap(&nums[j - 1], &nums[j]);
+        }
+    }
+}
